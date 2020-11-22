e6257424450b5a095cfe92fd07ee8d6af68af9a2
LarouTech <yanicklarouche@hotmail.com>
2020-11-21 19:25:53 -0500

    commit

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello People!')
+  res.send('Hello People!!!');
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(3000, () => {
+  console.log('Vas donc chier');
 })
